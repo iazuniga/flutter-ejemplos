01e25851b9c93c528372e4872753b7ece3b80644
iazuniga <114851104+iazuniga@users.noreply.github.com>
2022-10-24 22:09:46 -0700

    Add files via upload

---
+++ crudb/lib/presentation/pages/main_screen.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:crudb/presentation/pages/home_page.dart';
+
+import 'login_page.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return HomePage();
+          } else {
+            return LoginPage();
+          }
+        });
+  }
+}
